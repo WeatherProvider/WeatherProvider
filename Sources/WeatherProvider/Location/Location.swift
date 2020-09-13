e14426204f91643bb458da04ce27fe758055da9d
Alan Chu <ualch9@gmail.com>
2020-09-13 16:38:16 -0700

    Allow initializing Location using Geohash

--- Sources/WeatherProvider/Location/Location.swift
+++ Sources/WeatherProvider/Location/Location.swift
@@ -5,9 +5,11 @@
 //  Created by Alan Chu on 7/13/20.
 //
 
-import Foundation
+import GeohashKit
 
 public protocol Location {
     var latitude: Double { get }
     var longitude: Double { get }
+
+    init(geohash: Geohash)
 }
